2934bc25a7125524b0732099d9b11b3a4e907ab2
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-08-11 15:49:13 +0200

    created database structure

---
+++ db/tables.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS waiters (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS workingdays (
+    id SERIAL PRIMARY KEY,
+    waiter_id INTEGER NOT NULL,
+    workingday VARCHAR(10) NOT NULL,
+    FOREIGN KEY(waiter_id) REFERENCES waiters(id)
+);
+
+-- INSERT INTO waiters (username) VALUES ($1);
+-- INSERT INTO workingdays (waiter_id, workingday) VALUES((SELECT id FROM waiters WHERE username=$1 LIMIT 1), $2)
+-- SELECT waiter.username, workingdays.workingday FROM waiters INNER JOIN workingdays ON waiters.id = workingdays.waiter_id;
+-- SELECT waiter.username, workingdays.workingday FROM waiters INNER JOIN workingdays ON waiters.id = workingdays.waiter_id WHERE username = $1;
