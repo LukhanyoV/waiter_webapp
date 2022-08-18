fe364dbddfc6a16228d2f09b833f38998da4dbfa
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-08-18 22:05:34 +0200

    updated my table sql

--- db/tables.sql
+++ db/tables.sql
@@ -1,16 +1,24 @@
+DROP TABLE IF EXISTS waiters, weekdays, workingdays;
+
 CREATE TABLE IF NOT EXISTS waiters (
     id SERIAL PRIMARY KEY,
     username VARCHAR(20) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS weekdays (
+    id SERIAL PRIMARY KEY,
+    week_day VARCHAR(10) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS workingdays (
     id SERIAL PRIMARY KEY,
     waiter_id INTEGER NOT NULL,
-    workingday VARCHAR(10) NOT NULL,
-    FOREIGN KEY(waiter_id) REFERENCES waiters(id)
+    workingday INTEGER NOT NULL,
+    FOREIGN KEY(waiter_id) REFERENCES waiters(id),
+    FOREIGN KEY(workingday) REFERENCES weekdays(id)
 );
 
--- INSERT INTO waiters (username) VALUES ($1);
--- INSERT INTO workingdays (waiter_id, workingday) VALUES((SELECT id FROM waiters WHERE username=$1 LIMIT 1), $2)
--- SELECT waiters.username, workingdays.workingday FROM waiters INNER JOIN workingdays ON waiters.id = workingdays.waiter_id;
--- SELECT waiters.username, workingdays.workingday FROM waiters INNER JOIN workingdays ON waiters.id = workingdays.waiter_id WHERE username = $1;
+-- PREPOPULATE weekdays table
+INSERT INTO weekdays (week_day) 
+VALUES ('monday'), ('tuesday'), ('wednesday'), 
+('thursday'), ('friday'), ('saturday'), ('sunday');
