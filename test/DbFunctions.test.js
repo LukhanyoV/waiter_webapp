bd2af30bf25db7e8873f8537af8cc989286a2b96
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-08-19 12:45:21 +0200

    should be able to get weekdays

--- test/DbFunctions.test.js
+++ test/DbFunctions.test.js
@@ -24,10 +24,21 @@ describe("Testing my Waiter App queries", () => {
         const dbFunction = DbFunctions(db)
         await dbFunction.addWaiter("Lukhanyo")
 
+        // adding user to work for days mon-friday
+        // each day is represented by the id from 1-7
+        //  1 being monday - 7 being sunday
         await dbFunction.addWorkDay("Lukhanyo", [1,2,3,4,5])
 
         const days = await dbFunction.getDaysFor("Lukhanyo")
 
         assert.equal(5, await days.length)
+    })
+
+    it("Should be able to get weekdays stored in database", async () => {
+        const dbFunction = DbFunctions(db)
+
+        // there are 7 weekdays that are stored on the weekdays table4
+        const weekdays = await dbFunction.getWeekdays()
+        assert.equal(7, weekdays.length)
     })
 })
