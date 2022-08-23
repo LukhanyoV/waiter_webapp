704e45f43cd787b4c064d1e44ab7797200b118bb
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-08-23 12:41:17 +0200

    updated remove user route

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post("/clear", routes.clearWaiters)
 
 app.post("/clear/:day", routes.clearDay)
 
-app.post("/remove/:username", routes.removeUser)
+app.post("/days/:day", routes.removeUser)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`App running on PORT: ${PORT}`))
